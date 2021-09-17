61f1b3b485350efdab19c64ab6fe3fc936995595
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-09-17 17:42:06 +0000

    Stripe part 3 - Added checkout success logic - fixing the  last commit title

--- checkout/signals.py
+++ checkout/signals.py
@@ -12,5 +12,4 @@ def update_on_save(sender, instance, created, **kwargs):
 def update_on_delete(sender, instance, **kwargs):
     """ Update order total on lineitem delete """
 
-    print('delete signals RECEIVED!')
-    instance.order.update_total() 
+    instance.order.update_total()
