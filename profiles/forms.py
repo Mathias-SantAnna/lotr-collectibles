c5e65ee053977f77c56b42730a13c1663f38f40a
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-09-22 05:10:03 +0000

    Add confirmation email

--- profiles/forms.py
+++ profiles/forms.py
@@ -26,9 +26,9 @@ class UserProfileForm(forms.ModelForm):
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
+                    placeholder = f'{placeholders[field]}*'
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'profile--form--input'
+            self.fields[field].widget.attrs['class'] = 'profile-form-input'
             self.fields[field].label = False 
