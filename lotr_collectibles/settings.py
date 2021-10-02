5cbf5313028c3c741473e8ad3266ae50fb12b507
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-10-02 23:44:26 +0000

    Deployment part 1

--- lotr_collectibles/settings.py
+++ lotr_collectibles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-5&h3+%0&2sgvh^s%k#&u3+gpbjqaf56ru@cvxvao&t)=fd@@9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lotr-collectibles.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,11 +120,15 @@ WSGI_APPLICATION = 'lotr_collectibles.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://pixjmxhatrhngw:4efeae6795957ecea9a609d28e54974953156a497e358ecee6b2d3dda84df11d@ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/ddojbai7aa7o9u')
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
