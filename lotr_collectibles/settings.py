7b07c6f78cb96ac3e4baf07681b57b619a9d2fe3
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-10-03 03:05:48 +0000

    Removed secret key and set debug

--- lotr_collectibles/settings.py
+++ lotr_collectibles/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5&h3+%0&2sgvh^s%k#&u3+gpbjqaf56ru@cvxvao&t)=fd@@9d'
+SECRET_KEY = ('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lotr-collectibles.herokuapp.com', 'localhost']
 
