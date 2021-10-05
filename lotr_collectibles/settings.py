e275f69b67509ad5aec4111030740fe65ffd71a3
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-10-05 00:25:45 +0000

    Add page 404

--- lotr_collectibles/settings.py
+++ lotr_collectibles/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = ('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['lotr-collectibles.herokuapp.com', 'localhost']
 
