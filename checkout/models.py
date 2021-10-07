6b04e263698753646b2a5260be15e5f14f88ca35
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-10-07 17:38:18 +0000

    test models issue2

--- checkout/models.py
+++ checkout/models.py
@@ -50,7 +50,7 @@ class Order(models.Model):
         """
         # {'lineitem_total__sum': Decimal('149.90')}
         self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))'lineitem_total__sum'] or 0
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
