c1b005a51e8c5fffc7d305ede43d0289080f8035
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-10-07 17:37:15 +0000

    test models issue2

--- checkout/models.py
+++ checkout/models.py
@@ -48,11 +48,9 @@ class Order(models.Model):
         """Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))
-        ['lineitem_total__sum'] or 0
-        print("\n#######")
-        order_total=self.order_total
-        delivery_threshhold = settings.FREE_DELIVERY_THRESHOLD
+        # {'lineitem_total__sum': Decimal('149.90')}
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))'lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
