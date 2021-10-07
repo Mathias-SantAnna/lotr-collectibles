dc1d150590f4746f529c04405670193d779d2f82
Mathias-SantAnna <dark_tricolor@hotmail.com>
2021-10-07 16:36:45 +0000

    Change models.py in checkout

--- checkout/models.py
+++ checkout/models.py
@@ -32,7 +32,7 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
     free_collectible = models.BooleanField(default=False, null=True,
-                                          blank=True)
+                                           blank=True)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
@@ -51,8 +51,7 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))
         ['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.\
-                STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
